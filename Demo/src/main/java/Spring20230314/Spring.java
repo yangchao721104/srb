04ad5f54a868910240b299fb451e360437730835
yangchao <909833655@qq.com>
2023-03-15 00:17:23 +0800

    1

---
+++ Demo/src/main/java/Spring20230314/Spring.java
@@ -0,0 +1,19 @@
+package Spring20230314;
+
+/**
+ * @author yang
+ * @date 2023/3/14 13:54
+ */
+public class Spring {
+    //自增变量
+    public static void main(String[] args) {
+        int i = 1;
+        i = i++;
+//        int j = i++;
+//        int k = i + ++i *i++;
+        System.out.println("i = " + i);
+//        System.out.println("j = " + j);
+//        System.out.println("k = " + k);
+
+    }
+}
